87efbfbf456f15ad607109488a45d635542fa6a8
kuro <80121168@qq.com>
2019-03-05 11:12:11 +0800

    Update NoteImageFragment.java

--- app/src/main/java/com/shu/xxnote/fragment/NoteImageFragment.java
+++ app/src/main/java/com/shu/xxnote/fragment/NoteImageFragment.java
@@ -33,11 +33,12 @@ import cn.bmob.v3.listener.FindListener;
 
 public class NoteImageFragment extends BaseFragment implements CardStackView.ItemExpendListener{
     View rootView;
-    private CardStackView cardStackView;
-    private ImageButton button_add;
-    private TestStackAdapter mTestStackAdapter;
-    private Note[] notes;
-    private Integer[] DATAS;
+    CardStackView cardStackView;
+    ImageButton button_add;
+    TestStackAdapter mTestStackAdapter;
+    private int[] test = new int[]{1,2,3,4,5};
+    public Note[] notes;
+    public Integer[] DATAS;
     public static Integer[] TEST_DATAS = new Integer[]{
             R.color.color_1, R.color.color_2, R.color.color_3, R.color.color_4, R.color.color_5,
             R.color.color_6, R.color.color_7, R.color.color_8, R.color.color_9, R.color.color_10,
@@ -52,42 +53,48 @@ public class NoteImageFragment extends BaseFragment implements CardStackView.Ite
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = getSubView(inflater,container);
-        initView(rootView);
         initBmob();
         return rootView;
     }
 
     private void initBmob() {
         notebookId = ((NoteActivity)getActivity()).getNotebookId();
-        System.out.println("notebookId"+notebookId+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         Bmob.initialize(getActivity(),"b69650c5254cf30c803d7b958a002ef1");
         BmobQuery<Note> query = new BmobQuery<Note>();
-        BmobQuery<Notebook> innerQuery = new BmobQuery<Notebook>();
+        BmobQuery<Notebook> innerQuery = new BmobQuery<>();
         innerQuery.addWhereEqualTo("objectId",notebookId);
-        query.addWhereMatchesQuery("notebook", "Notebook", innerQuery);
+        Notebook notebook = new Notebook();
+        notebook.setObjectId(notebookId);
+        query.addWhereMatchesQuery("notebook","Notebook",innerQuery);
         query.findObjects(new FindListener<Note>() {
             @Override
                         public void done(List<Note> object, BmobException e) {
+                            System.out.println(object.size()+"!!!!!!!!!!!!!!!!!!!");
                             if(e==null){
-                                System.out.print("数量："+object.size()+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                                notes = new Note[object.size()];
+                                DATAS = new Integer[object.size()];
                                 for (int i=0;i<object.size();i++) {
-                                    //获得playerName的信息
+                                    System.out.println("i:"+i);
+                                    notes[i] = new Note();
                                     notes[i].setObjectId(object.get(i).getObjectId());
                                     notes[i].setType(object.get(i).getType());
                                     notes[i].setComment(object.get(i).getComment());
                                     notes[i].setNotebook(object.get(i).getNotebook());
-                    }
-                }else{
+                                    System.out.println("notes:"+notes[i].getComment());
+                                }
+
+                                int i=0,j=0;
+                                while (i<object.size()){
+                                    DATAS[i++]=TEST_DATAS[i];
+                                    if(j++ ==26)j=0;
+                                }
+                                initView(rootView);
+                            }else{
                                 System.out.println(e.getMessage()+","+e.getErrorCode());
                     //Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
-                }
+                            }
             }
         });
-        int j=0;
-        for (int i=0;i<notes.length;i++){
-            DATAS[i]=TEST_DATAS[j++];
-            if(j==25) j=0;
-        }
     }
 
     private void initView(View rootView) {
@@ -95,9 +102,9 @@ public class NoteImageFragment extends BaseFragment implements CardStackView.Ite
         button_add = (ImageButton)rootView.findViewById(R.id.imageButton3);
         cardStackView.setItemExpendListener(this);
         mTestStackAdapter = new TestStackAdapter(getActivity());
+        mTestStackAdapter.setNotes(notes);
         cardStackView.setAdapter(mTestStackAdapter);
         cardStackView.setAnimatorAdapter(new UpDownAnimatorAdapter(cardStackView));
-
 
         new Handler().postDelayed(
                 new Runnable() {
